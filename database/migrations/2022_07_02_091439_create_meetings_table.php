123ebc59fa772ad733d6e5e66ca472ff2ad09d01
shahreecoder <codershahree@gmail.com>
2022-07-02 14:45:22 +0500

    table_migration_Done

---
+++ database/migrations/2022_07_02_091439_create_meetings_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meetings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('name');
+            $table->string('location');
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->integer('meeting_time');
+            $table->integer('distance_time');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meetings');
+    }
+};
